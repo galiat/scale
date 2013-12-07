9a31cb1b1bb9302c917230d51ca6890b72f925ac
galia <galiat@gmail.com>
2013-12-07 00:28:02 -0500

    removed old path

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     unless @user.persisted?
       session['devise.withings_data'] = request.env['omniauth.auth']
-      return redirect_to new_user_registration_url
+      return redirect_to root_url
     end
 
     sign_in_and_redirect @user, event: :authentication
