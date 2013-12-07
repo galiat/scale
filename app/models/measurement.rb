dad15fe97879c213e12ca7efc710274dd9f4d21d
galia <galiat@gmail.com>
2013-12-07 01:34:24 -0500

    when making api call, no limits on page size and only get measuments not already saved

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -7,7 +7,8 @@ class Measurement < ActiveRecord::Base
   default_scope order('taken_at DESC')
 
   def self.collect_most_recent_measurements_for(user)
-    measurement_groups = user.withings_user.measurement_groups device: Withings::SCALE, per_page: 1000
+    measurements_after = user.measurements.first.taken_at + 1.second #need to +1 to not include the last measurement
+    measurement_groups = user.withings_user.measurement_groups device: Withings::SCALE, per_page: 0, start_at: measurements_after
     measurement_groups.each{|m| Measurement.where(user_id: user.id, weight: m.weight, taken_at: m.taken_at).first_or_create}
   end
 end
